e3b8185376955fc6177627fb682c807697e40219
Saumo Pal <saumopal1997@gmail.com>
2025-06-29 18:16:51 +0530

    Updated VoiceAssistant.tsx

--- src/components/Guide/VoiceAssistant.tsx
+++ src/components/Guide/VoiceAssistant.tsx
@@ -276,7 +276,7 @@ export function VoiceAssistant({ project, currentStep }: VoiceAssistantProps) {
                 size="sm"
                 variant="ghost"
                 onClick={stopSpeaking}
-                className="p-1 h-8 w-8 hover:bg-red-50"
+                className="bg-white p-1 h-8 w-8 hover:bg-red-50"
               >
                 <VolumeX className="w-4 h-4 text-red-600" />
               </Button>
